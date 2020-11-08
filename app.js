c59aca95379036591a2bd34d3808758f29060dc0
0dhapatel <39307304+0dhapatel@users.noreply.github.com>
2020-11-08 09:48:14 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+function sayHello(name){
+	console.log('Hello'+name);
+}
+
+sayHello('Dhaval');
+console.log(window);
+
+setTimeout()
+clearTimeout();
+
+setInterval()
+clearInterval()
+
+var mess='';
+global.setTimeout
+
